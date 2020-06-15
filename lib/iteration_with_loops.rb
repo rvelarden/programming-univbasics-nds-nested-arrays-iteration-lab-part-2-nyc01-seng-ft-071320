8c5c030865a6a7d12c687be8be5ed46996554ac2
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 22:29:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,16 @@
-def find_min_in_nested_arrays(src)
+
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-new_array = -1
-while new_array < src.count do
-    
-    if element_index < src[new_array].count do
-      
+def find_even_values(src)
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1
   end
 end
-end 
