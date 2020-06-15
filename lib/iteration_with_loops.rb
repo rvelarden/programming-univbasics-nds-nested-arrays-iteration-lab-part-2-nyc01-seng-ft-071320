e5c36362e2a64596c9c536a1f6b63da3ecc6c8c6
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 22:40:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def find_min_in_nested_arrays(src)
     while element_index < src[row_index].count do
       if src[row_index][element_index][0] == "P"
         outer_results << src[row_index][element_index]
-      end
+        end
       end
       element_index += 1
     end
