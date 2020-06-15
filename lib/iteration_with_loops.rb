172c97f81f66f9f912cc10c78b25f01abed4d67b
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 23:11:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,10 @@
-def find_min_in_nested_arrays(src)       
-    outer_results = []                  
+      
+  def find_min_in_nested_arrays(src)
+    array_of_daily_temp = []
     row_index = 0
-  while row_index < src.count do
-    element_index = 0
-    while element_index < src[row_index].count do
-      if src[row_index][element_index][0] == "P"
-        outer_results << src[row_index][element_index]
-      end
-      element_index += 1
-    end
-    row_index += 1
-  end
-outer_results
-end
+      while row_index < src.count do
+        element_index = 0
+        min_temp = 100
+          while element_index < src[row_index].count do
+            if src[row_index][element_index] < min_temp
+              min_temp = src[row_index][element_index]
